f682d794cd520a74989dc1ed9c322433b5fbf448
faizan-rauf05 <faizanrauf2525@gmail.com>
2024-08-01 18:07:42 +0500

    remove unnecessary images

--- src/pages/User/Flights.jsx
+++ src/pages/User/Flights.jsx
@@ -1,8 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import FlightsSearch from "./FlightsSearch";
+import FlightCard from "./FlightCard";
 
 const Flights = () => {
-  return <FlightsSearch />;
+  const [flightsData, setFlightsData] = useState([]);
+  const [flightsMeta, setFlightsMeta] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8
+
+  // Calculate the current items to display based on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentFlights = flightsData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Handle page change
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, Math.ceil(flightsData.length / itemsPerPage)));
+  };
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  return (
+    <>
+      <FlightsSearch
+        setFlightsData={setFlightsData}
+        setFlightsMeta={setFlightsMeta}
+      />
+      <h2 className="text-white text-center text-3xl my-4" >Real-Time Flights Data {flightsData.length}</h2>
+      <div className="w-full flex gap-4 flex-wrap">
+        {currentFlights.map((currFlight,index) => {
+          return (
+            <FlightCard
+              key={index}
+              {...currFlight}
+              flightsMeta={flightsMeta}
+            />
+          );
+        })}
+      </div>
+      <div className="pagination-controls mx-auto my-4">
+        <button onClick={handlePreviousPage} className="bg-[#09131f] rounded p-2 text-white mr-2" disabled={currentPage === 1}>
+          Previous
+        </button>
+        <span className="text-white" >Page {currentPage} of {Math.ceil(flightsData.length / itemsPerPage)}</span>
+        <button onClick={handleNextPage} className="bg-[#09131f] rounded p-2 text-white ml-2" disabled={currentPage === Math.ceil(flightsData.length / itemsPerPage)}>
+          Next
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Flights;
--- src/pages/User/FlightsSearch.jsx
+++ src/pages/User/FlightsSearch.jsx
@@ -1,68 +1,89 @@
 import React, { useState } from "react";
 import { get } from "../../services/apiEndpoint";
+import CircleLoader from "react-spinners/ClipLoader";
+import toast from "react-hot-toast";
 
-const FlightsSearch = () => {
+const FlightsSearch = ({ setFlightsData, setFlightsMeta }) => {
   const [searchData, setSearchData] = useState({
-    departurePlace: "",
-    arrivalPlace: "",
+    airportcode: "",
+    price: 0,
   });
+  const [loading, setLoading] = useState(false);
 
   const handleUserSearch = async (e) => {
     e.preventDefault();
-    const response = await get(
-      `/api/destinations?origin=PAR&maxPrice=100`
-    );
-    console.log(response)
-    // if (resposne.status == 200) {
-    //   setSearchData({
-        
-    //     departurePlace: "",
-    //     arrivalPlace: "",
-    //   });
-    // }
+    try {
+      setLoading(true);
+      const response = await get(
+        `/api/destinations?origin=${searchData.airportcode}&maxPrice=${searchData.price}`
+      );
+      if (response.status == 200) {
+        setLoading(false);
+        setFlightsData(response.data.data);
+        setFlightsMeta(response.data.meta);
+      }
+    } catch (error) {
+      if (error.response) {
+        toast.error(error.response.data.message || "No any data found");
+        setLoading(false);
+      }
+    }
   };
 
   return (
     <>
       <div className="bg-transparent p-6 rounded-lg w-full max-w-2xl mx-auto mt-2">
-        <form className="flex items-center justify-center flex-wrap -mx-2">
+        <form
+          className="flex items-center justify-center flex-wrap -mx-2"
+          onSubmit={handleUserSearch}
+        >
           <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
             <input
               type="text"
               placeholder="Airport Code"
-              name="departurePlace"
+              name="airportcode"
               className="w-full placeholderColor-white text-white p-3 bg-transparent border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0e1f33]"
-              // value={searchData.departurePlace}
-              // onChange={(e) =>
-              //   setSearchData({
-              //     ...searchData,
-              //     [e.target.name]: e.target.value,
-              //   })
-              // }
+              value={searchData.departurePlace}
+              onChange={(e) =>
+                setSearchData({
+                  ...searchData,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </div>
           <div className="w-full md:w-1/3 px-2 mb-4 md:mb-0">
             <input
               type="text"
               placeholder="Max Price"
-              name="arrivalPlace"
+              name="price"
               className="w-full p-3 bg-transparent text-white border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0e1f33]"
-              // value={searchData.arrivalPlace}
-              // onChange={(e) =>
-              //   setSearchData({
-              //     ...searchData,
-              //     [e.target.name]: e.target.value,
-              //   })
-              // }
+              value={searchData.arrivalPlace}
+              onChange={(e) =>
+                setSearchData({
+                  ...searchData,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </div>
           <div className=" px-2">
-            <button
-              onClick={handleUserSearch}
-              className="max-w-lg bg-[#152831] text-white p-3 rounded-lg hover:bg-[#0e1f33] focus:outline-none focus:ring-2 focus:ring-[#0e1f33]"
-            >
-              Search
-            </button>
+            {loading ? (
+              <CircleLoader
+                color={"#fff"}
+                size={30}
+                aria-label="Loading Spinner"
+                data-testid="loader"
+                className="ml-4"
+              />
+            ) : (
+              <button
+                type="submit"
+                className="w-full text-white bg-[#13253a] hover:bg-[#09131f] focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Search
+              </button>
+            )}
           </div>
         </form>
       </div>
